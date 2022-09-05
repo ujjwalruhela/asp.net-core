321d90ee959c3f9101f9af5d30fb7eeb9f359613
ujjwal <ujjwalruhela.a2020mcaq3237@imsgzb.ac.in>
2022-09-05 09:58:47 +0530

    Database Conectivity

---
+++ BookWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace BookWeb.Models
+{
+    public class Category
+    {
+      
+        [Key]           //data annotation
+        public int Id { get; set; }
+      
+        [Required]      //data annotation
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; }=DateTime.Now; 
+    }
+}
